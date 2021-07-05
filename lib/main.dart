2720f139a427c44eb3cb71f1990dfa3ada4a3ed3
Muhammad Ramzan <mRamzan.ga@gmail.com>
2021-07-05 17:47:57 +0500

    I am Rich images/and body practice

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home:Scaffold(
+        backgroundColor: Colors.blueGrey[200],
         appBar: AppBar(
-        title : Text('I Am Rich')
+        title : Center(child: Text('I Am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+            ),
+        ),
         ),
       ),
-    ),
   );
 }
